f875f42144b9c50cf0eebe5a882cee2348b0713f
Abhishek Swaroop Shrivastava <63967165+gh4abhi@users.noreply.github.com>
2020-11-27 02:18:57 +0530

    Added solutions of some Hackerrank problems

---
+++ Hackerrank Problems/C++/Basic Data Types.cpp
@@ -0,0 +1,21 @@
+// Problem Link - https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long m;
+    char ch;
+    float h;
+    double l;
+    cin>>n>>m>>ch>>h>>l;
+    cout<<n<<"\n"<<m<<"\n"<<ch<<"\n";
+    printf("%f\n",h);
+    printf("%lf",l);// Complete the code.
+    return 0;
+}
+
+
---
+++ Hackerrank Problems/C++/Input and Output.cpp
@@ -0,0 +1,20 @@
+// Problem Link - https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() 
+{
+    int a,b,c,sum;
+    cin>>a>>b>>c;
+    sum=a+b+c;
+    cout<<sum;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
