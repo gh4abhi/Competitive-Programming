9ea6c6e34ad1d06c7ff254851f31caf2b99d9344
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-10-08 02:05:15 +0530

    Solved one question.

--- Leetcode/Dynamic Programming/72. Edit Distance.cpp
+++ Leetcode/Dynamic Programming/72. Edit Distance.cpp
@@ -67,3 +67,42 @@ public:
   return ans;
     }
 };*/
+
+// DP Solution
+
+#define ll long long
+class Solution {
+public:
+    
+    int minDistance(string s, string t) {
+       ll m = s.length();
+    ll n = t.length();
+    ll dp[m+1][n+1];
+    for(ll i=0;i<=m;i++)
+    {
+        dp[i][0] = i;
+    }
+    for(ll i=0;i<=n;i++)
+    {
+        dp[0][i] = i;
+    }
+    for(ll i=1;i<=m;i++)
+    {
+        for(ll j=1;j<=n;j++)
+        {
+            if(s[m-i]==t[n-j])
+            {
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else {
+                ll a = dp[i-1][j];
+                ll b = dp[i][j-1];
+                ll c = dp[i-1][j-1];
+                dp[i][j] = 1 + min(a, min(b, c));
+            }
+        }
+    }
+   
+    return dp[m][n];
+    }
+};
