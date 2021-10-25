6696989f0f397f27c3ac7079fc7a151f28b5b902
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2021-10-25 22:32:28 +0530

    Added solution of one problem.

---
+++ Codeforces/Codeforces Round #620 (Div. 2)/B - Longest Palindrome.cpp
@@ -0,0 +1,384 @@
+// Problem Link - https://codeforces.com/contest/1304/problem/B
+
+/*-----------------------------------------------------------------
+                    Always beleive you can.
+-------------------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+#define BOOST  ios_base::sync_with_stdio(false);    cin.tie(NULL);
+#define empb emplace_back
+#define mpr make_pair
+#define sortit(v) sort(v.begin(), v.end())
+#define rev(v) reverse(v.begin(), v.end())
+#define issorted(v) is_sorted(v.begin(),v.end())
+#define prt1(a) cout<<a<<" ";
+#define prt2(a,b) cout<<a<<" "<<b<<" ";
+#define prt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<" ";
+#define ll long long
+#define vec  vector<ll>
+#define pb push_back
+#define pa vector<pair<ll,ll>>
+#define fi first
+#define se second
+#define siz size()
+#define full(a) a.begin(),a.end()
+#define max_val INT_MIN
+#define min_val INT_MAX;
+#define MOD 1000000007
+
+
+using namespace std;
+
+//----------------------------------------------------------------Necessary Functions---------------------------------------------------------------------------------------
+void printvec(vector<ll> v) {
+    for(auto x:v)
+        cout<<x<<" ";
+}
+
+ll fact(ll n)   {
+    ll m=1;
+    for(ll i=1;i<=n;i++)
+        m*=i;
+    return m;
+}
+
+void inOt()
+{
+    freopen("input.txt" , "r" , stdin);   // For getting input from input.txt file
+    freopen("output.txt" , "w" , stdout);  // Printing the Output to output.txt file
+}
+ll lcm (ll a,ll b)
+{
+    return (a / __gcd(a, b)) * b;
+}
+
+ll power(ll x, unsigned ll y, unsigned ll m)
+{
+    if (y == 0)
+        return 1;
+    ll po = power(x, y / 2, m) % m;
+    po = (po * po) % m;
+
+    return (y % 2 == 0) ? po : (x * po) % m;
+}
+
+
+bool isPrime(ll n){
+    if (n <= 1)
+        return false;
+
+    for (ll i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+ll modInverse(ll a,ll m)
+{
+    return power(a, m - 2, m);
+
+}
+
+vector<ll> primeFactors(ll n)
+{
+    vec v2;
+    while (n % 2 == 0)
+    {
+        v2.pb(2);
+        n = n/2;
+    }
+
+    for (ll i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            v2.pb(i);
+            n = n/i;
+        }
+    }
+
+    if (n > 2)
+        v2.pb(n);
+    return v2;
+}
+vector<ll> pr;
+ll h = 1;
+ll st = 19000;
+bool prime[100];
+vector<ll> SieveOfEratosthenes(ll n)
+{
+    // Create a boolean array
+    // "prime[0..n]" and initialize
+    // all entries it as true.
+    // A value in prime[i] will
+    // finally be false if i is
+    // Not a prime, else true.
+    memset(prime, true, sizeof(prime));
+
+    for (ll p = 2; p * p <= n; p++)
+    {
+        // If prime[p] is not changed,
+        // then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples
+            // of p greater than or
+            // equal to the square of it
+            // numbers which are multiple
+            // of p and are less than p^2
+            // are already been marked.
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    vector<ll> vect;
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            vect.pb(p);
+    return vect;
+}
+int LCSubStr(char* X, char* Y, int m, int n)
+{
+    // Create a table to store
+    // lengths of longest
+    // common suffixes of substrings.
+    // Note that LCSuff[i][j] contains
+    // length of longest common suffix
+    // of X[0..i-1] and Y[0..j-1].
+
+    int LCSuff[m + 1][n + 1];
+    int result = 0; // To store length of the
+    // longest common substring
+
+    /* Following steps build LCSuff[m+1][n+1] in
+        bottom up fashion. */
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            // The first row and first column
+            // entries have no logical meaning,
+            // they are used only for simplicity
+            // of program
+            if (i == 0 || j == 0)
+                LCSuff[i][j] = 0;
+
+            else if (X[i - 1] == Y[j - 1]) {
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                result = max(result, LCSuff[i][j]);
+            }
+            else
+                LCSuff[i][j] = 0;
+        }
+    }
+    return result;
+}
+bool compare(const pair<ll, ll>&i, const pair<ll, ll>&j)
+{
+    if(i.second != j.second)
+    {
+        return i.second > j.second;
+    }
+    /*else
+    {
+        return i.first < j.first;
+    }*/
+}
+int isSubstring(string s1, string s2)
+{
+    int M = s1.length();
+    int N = s2.length();
+
+    /* A loop to slide pat[] one by one */
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+
+        /* For current index i, check for
+ pattern match */
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+
+        if (j == M)
+            return i;
+    }
+
+    return -1;
+}
+string decToBinary(int n)
+{
+    string ans="";
+    // Size of an integer is assumed to be 32 bits
+    for (int i = 31; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            ans+="1";
+        else
+            ans+="0";
+    }
+    return ans;
+}
+long long gcd(long long int a, long long int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+// Function to return LCM of two numbers
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+
+ll checkPalindrome(string str)
+{
+    ll n = str.length();
+    ll start = 0;
+    ll end = 0;
+    if(n&1)
+    {
+        start = (n+1)/2;
+        end = (n+1)/2;
+    }
+    else
+    {
+        start = n/2;
+        end = n/2+1;
+    }
+    start--;
+    end--;
+    ll count = 0;
+    while(start>=0 and end<n)
+    {
+        if(str[start]!=str[end])
+        {
+            count++;
+        }
+        start--;
+        end++;
+    }
+    if(count==0) {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+//------------------------------------------------------------------------------Code---------------------------------------------------------------------------------
+
+void its_Function() {
+    ll n,m;
+    cin>>n>>m;
+    vector<string> palindromes, nonPalindromes;
+    vector<string> vect(n);
+    for(ll i=0;i<n;i++)
+    {
+        cin>>vect[i];
+        if(checkPalindrome(vect[i]))
+        {
+            palindromes.pb(vect[i]);
+        }
+        else
+        {
+            nonPalindromes.pb(vect[i]);
+        }
+    }
+    /*for(auto i:nonPalindromes)
+    {
+//        cout<<"i "<<i<<endl;
+    }*/
+//    nonPalindromes = vect;
+    string ans="", revAns = "";
+    map<string,ll> m1;
+    for(ll i=0;i<nonPalindromes.size();i++)
+    {
+        string temp = nonPalindromes[i];
+        if(m1.count(temp)>0)
+        {
+            continue;
+        }
+        reverse(full(temp));
+//        cout<<temp<<endl;
+        if(m1.count(temp)>0)
+        {
+            continue;
+        }
+        ll ind = find(full(nonPalindromes),temp) - nonPalindromes.begin();
+        if(ind<nonPalindromes.size())
+        {
+            ans+=nonPalindromes[i];
+            m1[nonPalindromes[i]]++;
+            revAns = temp + revAns;
+            m1[temp]++;
+
+        }
+    }
+    string fPal, ePal;
+    map<string,ll> m2;
+    for(auto i: palindromes)
+    {
+        m2[i]++;
+    }
+    string midPal = "";
+    ll flag = 0;
+    for(auto i:m2)
+    {
+        if(i.second==1 and i.first.length()>midPal.length())
+        {
+            midPal = i.first;
+            flag=1;
+            continue;
+        }
+        for(ll j=0;j<i.second/2;j++)
+        {
+            fPal += i.first;
+            ePal += i.first;
+        }
+    }
+
+
+
+    string result ="";
+    result +=ans;
+    result += fPal;
+    result += midPal;
+    /*if(palindromes.size()>0)*/
+  /*  result+= palindromes[0];*/
+    result += ePal;
+    result += revAns;
+    cout<<result.length()<<endl<<result;
+}
+
+//--------------------------------------------------------------Main Function----------------------------------------------------------------------------------------------
+int main()
+{
+
+    BOOST;
+#ifndef ONLINE_JUDGE
+    inOt();
+#endif
+    cout<<fixed;
+   /* ll t;
+    cin>>t;
+    while(t--)
+    {
+
+        its_Function();
+        cout<<endl;
+    }*/
+    its_Function();return 0;
+}
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
