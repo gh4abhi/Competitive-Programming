5cd3e3e751ce10ba98e6b5ecb3ba714529fa5969
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-02-15 00:16:07 +0530

    Modified solution of one problem.

--- Leetcode/Dynamic Programming/97. Interleaving String.cpp
+++ Leetcode/Dynamic Programming/97. Interleaving String.cpp
@@ -68,7 +68,7 @@ public:
 
 // Tabulation Approach (2D DP)
 
-#define ll int
+/*#define ll int
 
 class Solution {
 public:
@@ -106,5 +106,43 @@ public:
             }
         }
         return dp[n][m];
+    }
+};*/
+
+// Tabulation (1D DP)
+
+#define ll int
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        ll n = s1.length();
+        ll m = s2.length();
+        if(m+n!=s3.length())
+            return false;
+        vector<bool> dp(m+1,false);
+        for(ll i=0;i<n+1;i++)
+        {
+            for(ll j=0;j<m+1;j++)
+            {
+                if(i==0 and j==0)
+                {
+                    dp[j] = true;
+                }
+                else if(i==0)
+                {
+                    dp[j] = dp[j-1] and s2[j-1]==s3[i+j-1];
+                }
+                else if(j==0)
+                {
+                    dp[j] = dp[j] and s1[i-1]==s3[i+j-1];
+                }
+                else
+                {
+                    dp[j] = (dp[j-1] and s2[j-1]==s3[i+j-1]) or (dp[j] and s1[i-1]==s3[i+j-1]);
+                }
+            }
+        }
+        return dp[m];
     }
 };
