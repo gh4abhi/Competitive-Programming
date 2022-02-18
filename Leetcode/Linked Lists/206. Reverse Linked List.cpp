84d71572817bf7878e43c4fea4b5fde904926286
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-02-18 06:24:21 +0530

    Added solution of one problem.

--- Leetcode/Linked Lists/206. Reverse Linked List.cpp
+++ Leetcode/Linked Lists/206. Reverse Linked List.cpp
@@ -1,5 +1,39 @@
 // Problem Link - https://leetcode.com/problems/reverse-linked-list/
 
+// Normal Method
+
+// #define ll int
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/*class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr or head->next == nullptr)
+             return head;
+        ListNode* pre = nullptr, *fow = nullptr, *cur = head;
+        while(cur!=nullptr)
+        {
+            fow = cur->next;
+            cur->next=pre;
+            pre = cur;
+            cur = fow;
+        }
+        return pre;
+    }
+};
+*/
+
+// Add First Method
+
 #define ll int
 
 /**
@@ -14,17 +48,32 @@
  */
 class Solution {
 public:
+    ListNode* th = nullptr, *tt = nullptr;
+    void addFirst(ListNode*node)
+        {
+            if(th==nullptr)
+            {
+                th = node;
+                tt = node;
+            }
+            else
+            {
+                node->next = th;
+                th = node;
+            }
+        }
+        
     ListNode* reverseList(ListNode* head) {
         if(head==nullptr or head->next == nullptr)
              return head;
-        ListNode* pre = nullptr, *fow = nullptr, *cur = head;
+        ListNode* cur = head,*fow= nullptr;
         while(cur!=nullptr)
         {
             fow = cur->next;
-            cur->next=pre;
-            pre = cur;
+            cur->next=nullptr;
+            addFirst(cur);
             cur = fow;
         }
-        return pre;
+        return th;
     }
 };
