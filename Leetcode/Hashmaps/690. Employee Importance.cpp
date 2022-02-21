62ac3d26b3cf834f0040e92416828a1cc21cb5d5
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-02-21 20:24:40 +0530

    Added solution of one problem.

--- Leetcode/Hashmaps/690. Employee Importance.cpp
+++ Leetcode/Hashmaps/690. Employee Importance.cpp
@@ -1,16 +1,6 @@
 // Problem Link - https://leetcode.com/problems/employee-importance/
 
-#define ll int
-
-/*
-// Definition for Employee.
-class Employee {
-public:
-    int id;
-    int importance;
-    vector<int> subordinates;
-};
-*/
+/*#define ll int
 
 class Solution {
 public:
@@ -43,4 +33,79 @@ public:
         }
         return sum;
     }
-};
+};*/
+
+// Tree Method
+
+
+#include <bits/stdc++.h>
+#define BOOST  ios_base::sync_with_stdio(false);    cin.tie(NULL);
+#define empb emplace_back
+#define mpr make_pair
+#define sortit(v) sort(v.begin(), v.end())
+#define forl(n) for(ll i=0;i<n;i++)
+#define prtIn(vect,i) cout<<vect[i]
+#define vecs(vect,n) vector<ll> vect(n)
+#define rev(v) reverse(v.begin(), v.end());
+#define issorted(v) is_sorted(v.begin(),v.end())
+#define prt1(a) cout<<a<<" ";
+#define prt2(a,b) cout<<a<<" "<<b<<" ";
+#define prt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<" ";
+#define ll long long
+#define vec  vector<ll>
+#define pb push_back
+#define pa vector<pair<ll,ll>>
+#define fi first
+#define se second
+#define sz size()
+#define full(a) a.begin(),a.end()
+#define max_val INT_MIN
+#define min_val INT_MAX;
+#define MOD 1000000007
+using namespace std;
+
+
+map<char,ll> ans;
+
+ll DFS(char ch, map<char,vector<char>> m)
+ {
+   if(m.count(ch)==0)
+   {
+     ans[ch] = 0;
+     return 1;
+   }
+   ll val = 0; 
+   for(auto i:m[ch])
+   {
+     val += DFS(i,m);
+   }
+
+   ans[ch] = val;
+   return (val+1);
+ }
+
+int main()
+{
+   ll n;
+ cin>>n;
+ map<char, vector<char>> m;
+ char ceo;
+ for(ll i=0;i<n;i++)
+ {
+   char a,b;
+   cin>>a>>b;
+   if(a==b)
+    ceo = b;
+   else
+    m[b].pb(a); 
+ }
+
+  ans[ceo] = DFS(ceo,m)-1;
+  for(auto i:ans)
+  {
+        cout<<i.first<<" "<<i.second<<endl;
+  }
+
+return 0;
+    
+}
