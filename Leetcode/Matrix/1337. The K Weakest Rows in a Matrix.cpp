73ddc66259d130fd0f5825736a470cc24f0735ee
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-04-02 04:13:50 +0530

    Added solution of one problem.

---
+++ Leetcode/Matrix/1337. The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,30 @@
+// Problem Link - https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+
+#define ll int
+
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<ll> ans;
+        vector<pair<ll,ll>> v;
+        ll ind = 0;
+        for(auto i:mat)
+        {
+            ll count = 0;
+            for(auto j:i)
+            {
+                if(j==0)
+                    break;
+                count++;
+            }
+            v.push_back({count,ind});
+                ind++;
+        }
+        sort(v.begin(),v.end());
+        for(ll i=0;i<k;i++)
+        {
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
