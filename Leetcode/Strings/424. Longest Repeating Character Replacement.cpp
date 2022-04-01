ac0c1060f0512c0775f3909c1701e0cdb33717a5
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-04-02 04:28:16 +0530

    Added solution of one problem.

---
+++ Leetcode/Strings/424. Longest Repeating Character Replacement.cpp
@@ -0,0 +1,27 @@
+// Problem Link - https://leetcode.com/problems/longest-repeating-character-replacement/
+
+#define ll int
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        ll start = 0;
+        ll end = 0;
+        map<char,ll> m;
+        ll mostFreq = 0;
+        ll maxLen = 0;
+        while(end<s.length())
+        {
+            m[s[end]]++;
+            mostFreq = max(m[s[end]], mostFreq);
+            if(end-start+1-mostFreq>k)
+            {
+                m[s[start]]-=1;
+                start++;
+            }
+            maxLen = max(maxLen,end-start+1);
+            end++;
+        }
+        return maxLen;
+    }
+};
