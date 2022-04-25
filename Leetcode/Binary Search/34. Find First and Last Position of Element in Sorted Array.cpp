38614f70dd0009386e207e03c8cac1df5f54c69d
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-04-25 06:33:35 +0530

    Added solution of one problem.

--- Leetcode/Binary Search/34. Find First and Last Position of Element in Sorted Array.cpp
+++ Leetcode/Binary Search/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -12,26 +12,17 @@ public:
         while(start<=end)
         {
             ll mid = start + (end-start)/2;
-            
             if(nums[mid]>target)
-            {
                 end = mid-1;
-            }
             else if(nums[mid]<target)
-            {
                 start = mid + 1;
-            }
             else
             {
                 ans = mid;
                 if(check)
-                {
                     end = mid - 1;
-                }
                 else
-                {
                     start = mid + 1;
-                }
             }
         }
         return ans;
