cb733ed46899475cd7317bde286c6cc01309b27f
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-06-25 17:50:13 +0530

    Added solution of one problem.

---
+++ Hackerrank Problems/Java/Java Substring.java
@@ -0,0 +1,19 @@
+// Problem Link - https://www.hackerrank.com/challenges/java-substring/problem?isFullScreen=true
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String str = S.substring(start,end);
+        System.out.print(str);
+    }
+}
