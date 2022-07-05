c0d7f9fb7e399e12bba248a5b189ec0d414ef360
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-07-06 02:03:21 +0530

    Added solution of one problem.

--- CodeStudio/Dynamic Programming/Maximum Path Sum in the matrix - Tabulation.cpp
+++ CodeStudio/Dynamic Programming/Maximum Path Sum in the matrix - Tabulation.cpp
@@ -1,99 +1,29 @@
-#include <bits/stdc++.h>
-#define SPEED  ios_base::sync_with_stdio(false);    cin.tie(NULL);
-#define empb emplace_back
-#define mpr make_pair
-#define sortit(v) sort(v.begin(), v.end())
-#define forl(n) for(ll i=0;i<n;i++)
-#define prtIn(vect,i) cout<<vect[i]
-#define vecs(vect,n) vector<ll> vect(n)
-#define rev(v) reverse(v.begin(), v.end());
-#define issorted(v) is_sorted(v.begin(),v.end())
-#define prt1(a) cout<<a<<" ";
-#define prt2(a,b) cout<<a<<" "<<b<<" ";
-#define prt3(a,b,c) cout<<a<<" "<<b<<" "<<c<<" ";
-#define ll long long
-#define vec  vector<ll>
-#define pb push_back
-#define pa vector<pair<ll,ll>>
-#define fi first
-#define se second
-#define sz size()
-#define full(a) a.begin(),a.end()
-#define max_val INT_MIN
-#define min_val INT_MAX;
-#define MOD 1000000007
- 
-using namespace std;
- 
-void inOt()
+// Problem Link - https://www.codingninjas.com/codestudio/problems/maximum-path-sum-in-the-matrix_797998
+
+#include<bits/stdc++.h>
+#define ll int
+int getMaxPathSum(vector<vector<int>> &matrix)
 {
-    freopen("input.txt" , "r" , stdin);
-    freopen("output.txt" , "w" , stdout);
-}
- 
-bool isPwerOf2(ll x)
-{
-    return x && (!(x&(x-1)));
-}
- 
- 
-bool cmp(pair<char,ll> a, pair<char,ll> b)
-{
-    if(a.second!=b.second)
-        return a.second>b.second;
-    return a.first>b.first;    
-}
- 
-void solve()
-{
-    ll n;
-    cin>>n;
-    vector<ll> vect(n);
+    ll m = matrix.size();
+    ll n = matrix[0].size();
+    ll dp[m][n];
     for(ll i=0;i<n;i++)
-    vect[i] = i+1;
-    
-    vector<ll> temp = vect;
-    temp[0] = 3;
-    temp[1] = 1;
-    temp[2] = 2;
-    for(auto i:temp)
-    cout<<i<<" ";
-    cout<<endl;
-    temp[0] = 3;
-    temp[1] = 2;
-    temp[2] = 1;
-    for(auto i:temp)
-    cout<<i<<" ";
-    cout<<endl;
-    ll k = n-2;
-    ll i=0,j=n-1;
-    while(k--)
+        dp[0][i] = matrix[0][i];
+    for(ll i=1;i<m;i++)
     {
-        if(i>=j)
+        for(ll j=0;j<n;j++)
         {
-            i = 0;
-            j = n-1;
+            ll one = -1e9, two = -1e9, three = -1e9;
+            one = dp[i-1][j];
+            if(j-1>=0)
+                two = dp[i-1][j-1];
+            if(j+1<n)
+                three = dp[i-1][j+1];
+            dp[i][j] = matrix[i][j] + max(one,max(two,three));
         }
-        swap(vect[i], vect[j]);
-        i++;
-        j--;
-        for(auto r:temp)
-            cout<<r<<" ";
-            cout<<endl;
     }
-}
- 
-int main()
-{
-    SPEED;
-    #ifndef ONLINE_JUDGE
-    inOt(); 
-    #endif
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        solve();
-    }
-    
+    ll ans = -1e9;
+    for(ll i=0;i<n;i++)
+        ans = max(ans,dp[m-1][i]);
+    return ans;
 }
