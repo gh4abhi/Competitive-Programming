b5e726ad98e131aeb4d8c33c7c72e1c2d46d7674
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-07-06 02:45:51 +0530

    Added solution of one problem.

--- Leetcode/Dynamic Programming/1289. Minimum Falling Path Sum II.cpp
+++ Leetcode/Dynamic Programming/1289. Minimum Falling Path Sum II.cpp
@@ -11,14 +11,21 @@ public:
             dp[i] = grid[0][i];
         for(ll i=1;i<m;i++)
         {
-            vector<ll> temp = dp;
+            ll mini = INT_MAX-1;
+            ll sMini = INT_MAX;
+            for(ll j=0;j<n;j++)
+            {
+                if(dp[j]<mini)
+                    sMini = mini, mini = dp[j];
+                else
+                    sMini = min(sMini,dp[j]);
+            }
            for(ll j=0;j<n;j++)
            {
-               ll val = 1e9;
-               for(ll k=0;k<n;k++)
-                   if(k!=j)
-                       val = min(val,temp[k]);
-               dp[j] = grid[i][j] + val;
+               if(dp[j]==mini)
+                   dp[j] = sMini + grid[i][j];
+                else
+                    dp[j] = mini + grid[i][j];
            }
         }
         ll ans = 1e9;
