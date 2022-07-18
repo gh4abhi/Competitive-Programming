d9899acab3b70f423fefdd5efdee490da886db3b
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-07-18 23:07:37 +0530

    Added solution of one problem.

--- Codechef/July Lunchtime 2022 Division 4 (Rated)/The Last Levels.cpp
+++ Codechef/July Lunchtime 2022 Division 4 (Rated)/The Last Levels.cpp
@@ -1,5 +1,11 @@
+// Problem Link - https://www.codechef.com/submit/LASTLEVELS?tab=statement
+
+/*-----------------------------------------------------------------
+                    Gamers Never Quit.
+-------------------------------------------------------------------*/
+ 
 #include <bits/stdc++.h>
-#define SPEED  ios_base::sync_with_stdio(false);    cin.tie(NULL);
+#define BOOST  ios_base::sync_with_stdio(false);    cin.tie(NULL);
 #define empb emplace_back
 #define mpr make_pair
 #define sortit(v) sort(v.begin(), v.end())
@@ -23,77 +29,333 @@
 #define min_val INT_MAX;
 #define MOD 1000000007
  
+ 
 using namespace std;
+ 
+//----------------------------------------------------------------Necessary Functions---------------------------------------------------------------------------------------
+void printvec(vector<ll> v) {
+    for(auto x:v)
+        cout<<x<<" ";
+}
  
 void inOt()
 {
     freopen("input.txt" , "r" , stdin);
     freopen("output.txt" , "w" , stdout);
 }
- 
-bool isPwerOf2(ll x)
+ll lcm (ll a,ll b)
 {
-    return x && (!(x&(x-1)));
+    return (a / __gcd(a, b)) * b;
+}
+ 
+ll power(ll x, unsigned ll y, unsigned ll m)
+{
+    if (y == 0)
+        return 1;
+    ll po = power(x, y / 2, m) % m;
+    po = (po * po) % m;
+ 
+    return (y % 2 == 0) ? po : (x * po) % m;
 }
  
  
-bool cmp(pair<char,ll> a, pair<char,ll> b)
-{
-    if(a.second!=b.second)
-        return a.second>b.second;
-    return a.first>b.first;    
+bool isPrime(ll n){
+    if (n <= 1)
+        return false;
+ 
+    for (ll i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+ 
+    return true;
 }
  
-void solve()
+ll modInverse(ll a,ll m)
 {
-    ll n;
-    cin>>n;
-    vector<ll> vect(n);
-    for(ll i=0;i<n;i++)
-    vect[i] = i+1;
-    
-    vector<ll> temp = vect;
-    temp[0] = 3;
-    temp[1] = 1;
-    temp[2] = 2;
-    for(auto i:temp)
-    cout<<i<<" ";
-    cout<<endl;
-    temp[0] = 3;
-    temp[1] = 2;
-    temp[2] = 1;
-    for(auto i:temp)
-    cout<<i<<" ";
-    cout<<endl;
-    ll k = n-2;
-    ll i=0,j=n-1;
-    while(k--)
+    return power(a, m - 2, m);
+ 
+}
+ 
+vector<ll> primeFactors(ll n)
+{
+    vec v2;
+    while (n % 2 == 0)
     {
-        if(i>=j)
+        v2.pb(2);
+        n = n/2;
+    }
+ 
+    for (ll i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
         {
-            i = 0;
-            j = n-1;
+            v2.pb(i);
+            n = n/i;
         }
-        swap(vect[i], vect[j]);
-        i++;
-        j--;
-        for(auto r:temp)
-            cout<<r<<" ";
-            cout<<endl;
+    }
+ 
+    if (n > 2)
+        v2.pb(n);
+    return v2;
+}
+vector<ll> pr;
+ll h = 1;
+ll st = 19000;
+bool prime[100];
+vector<ll> SieveOfEratosthenes(ll n)
+{
+    memset(prime, true, sizeof(prime));
+ 
+    for (ll p = 2; p * p <= n; p++)
+    {
+        // If prime[p] is not changed,
+        // then it is a prime
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+    vector<ll> vect;
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            vect.pb(p);
+    return vect;
+}
+int LCSubStr(char* X, char* Y, int m, int n)
+{
+    int LCSuff[m + 1][n + 1];
+    int result = 0; // To store length of the
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0 || j == 0)
+                LCSuff[i][j] = 0;
+ 
+            else if (X[i - 1] == Y[j - 1]) {
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                result = max(result, LCSuff[i][j]);
+            }
+            else
+                LCSuff[i][j] = 0;
+        }
+    }
+    return result;
+}
+int isSubstring(string s1, string s2)
+{
+    int M = s1.length();
+    int N = s2.length();
+ 
+    /* A loop to slide pat[] one by one */
+    for (int i = 0; i <= N - M; i++) {
+        int j;
+ 
+        /* For current index i, check for
+ pattern match */
+        for (j = 0; j < M; j++)
+            if (s2[i + j] != s1[j])
+                break;
+ 
+        if (j == M)
+            return i;
+    }
+ 
+    return -1;
+}
+string decToBinary(int n)
+{
+    string ans="";
+    // Size of an integer is assumed to be 32 bits
+    for (int i = 31; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            ans+="1";
+        else
+            ans+="0";
+    }
+    return ans;
+}
+long long gcd(long long int a, long long int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ 
+// Function to return LCM of two numbers
+long long lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+ll checkPalindrome(string str)
+{
+    ll n = str.length();
+    ll start = 0;
+    ll end = 0;
+    if(n&1)
+    {
+        start = (n+1)/2;
+        end = (n+1)/2;
+    }
+    else
+    {
+        start = n/2;
+        end = n/2+1;
+    }
+    start--;
+    end--;
+    ll count = 0;
+    while(start>=0 and end<n)
+    {
+        if(str[start]!=str[end])
+        {
+            count++;
+        }
+        start--;
+        end++;
+    }
+    if(count==0) {
+        return 1;
+    }
+    else
+    {
+        return 0;
     }
 }
  
+ll fact(ll n)
+{
+    ll res = 1;
+    for (ll i = 2; i <= n; i++)
+        res = res * i;
+    return res;
+}
+ 
+ 
+ll nCr(ll n, ll r)
+{
+    return fact(n) / (fact(r) * fact(n - r));
+}
+ 
+bool compare(const pair<ll, ll>&i, const pair<ll, ll>&j)
+{
+    if(i.second != j.second)
+    {
+        return i.second > j.second;
+    }
+    else
+    {
+        return i.first > j.first;
+    }
+}
+ 
+class threePair
+{
+public:
+    ll l;
+    ll r;
+    ll c;
+};
+ 
+pair<ll,ll> solve(ll n)
+{
+    ll count = 0;
+    cin>>n;
+    ll c = 0;
+    while (n) {
+        count += n & 1;
+        n >>= 1;
+        c++;
+    }
+    return make_pair(count,(c-count));
+}
+ 
+class Edge{
+public:
+    ll pre;
+    ll pos;
+    ll val;
+    Edge(ll pre, ll pos, ll val)
+    {
+        this->pre = pre;
+        this->pos = pos;
+        this->val = val;
+    }
+};
+ 
+bool isPalindrome(string S)
+{
+    string P = S;
+ 
+    reverse(P.begin(), P.end());
+ 
+    if (S == P) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+ 
+ll mex(vector<ll>& vect, ll i, ll j)
+{
+    ll ind = 0;
+    map<ll,ll> m;
+    for(ll k=i;k<=j;k++)
+    {
+        m[vect[k]]++;
+    }
+    while(1)
+    {
+        if(m.count(ind)==0)
+        return ind;
+        ind++;
+    }
+}
+void io(ll r)
+{
+    if(r==0)
+    return;
+}
+//------------------------------------------------------------------Code--------------------------------------------------------------------------------------------
+ 
+void its_Function() {
+ ll x,y,z;
+ cin>>x>>y>>z;
+ ll sum = x*y;
+ ll one = x/3;
+ if(x%3==0)
+    one--;
+if(one>0)
+    sum += z*one;
+cout<<sum;
+}
+ 
+//--------------------------------------------------------------Main Function----------------------------------------------------------------------------------------------
 int main()
 {
-    SPEED;
-    #ifndef ONLINE_JUDGE
-    inOt(); 
-    #endif
+ 
+    BOOST;
+/*#ifndef ONLINE_JUDGE
+    inOt();
+#endif*/
+    cout<<fixed;
     ll t;
     cin>>t;
     while(t--)
     {
-        solve();
+        its_Function();
+        cout<<endl;
     }
-    
+//    its_Function();
+ 
+    return 0;
 }
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ 
+ 
