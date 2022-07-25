f555adbb36f0be287a44d773eee4231ee3a1ecf3
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-07-26 03:02:03 +0530

    Added solution of one problem

--- Leetcode/Trees/Binary Trees/236. Lowest Common Ancestor of a Binary Tree.cpp
+++ Leetcode/Trees/Binary Trees/236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -11,85 +11,20 @@
  */
 class Solution {
 public:
-    
-    
-    pair<TreeNode*, pair<bool,bool>> lowestCommonAncestorHelper(TreeNode* root, TreeNode* p, TreeNode* q)
+    TreeNode* DFS(TreeNode* root, TreeNode* p, TreeNode* q)
     {
-        TreeNode* val = nullptr;
-        TreeNode* lVal = nullptr;
-        TreeNode* rVal = nullptr;
-        bool node1L = false, node1R = false, node2L = false, node2R = false;
-
-        pair<TreeNode*,pair<bool,bool>> l,r;
-        bool rootNodeCheck1 = false, rootNodeCheck2 = false;
-
-        if(root->val == p->val)
-        {
-            rootNodeCheck1 = true;
-        }
-        if(root->val == q->val)
-        {
-            rootNodeCheck2 = true;
-        }
-        if(root->left)
-        {      
-            l = lowestCommonAncestorHelper(root->left,p,q);
-            lVal = l.first;
-            node1L = l.second.first;
-            node2L = l.second.second;
-        }
-        if(root->right)
-        {
-            r = lowestCommonAncestorHelper(root->right,p,q);
-            rVal = r.first;
-            node1R = r.second.first;
-            node2R = r.second.second;
-        }
-            rootNodeCheck1 = rootNodeCheck1 or node1L or node1R;
-            rootNodeCheck2 = rootNodeCheck2 or node2L or node2R;
-
-    
-        if(rootNodeCheck2 and rootNodeCheck1 and lVal== nullptr and rVal==nullptr)
-        {
-        val = root;
-        }
-    
-        if(lVal)
-        {
-            val = lVal;
-        }
-        else if(rVal)
-        {
-            val = rVal;
-        }
-            return make_pair(val, make_pair(rootNodeCheck1,rootNodeCheck2));
-
-       }
-    
+        if(root==nullptr or root==p or root==q)
+            return root;
+        TreeNode* lef = DFS(root->left,p,q);
+        TreeNode* rig = DFS(root->right,p,q);
+        if(!lef)
+            return rig;
+        else if(!rig)
+            return lef;
+        else
+            return root;
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    TreeNode* val;
-    bool rootNodeCheck1, rootNodeCheck2;
-    pair<TreeNode*,pair<bool,bool>> ans;
-    ans = lowestCommonAncestorHelper(root, p, q);
-    val = ans.first;
-    rootNodeCheck1 = ans.second.first;
-    rootNodeCheck2 = ans.second.second;
-    if(rootNodeCheck1 and !rootNodeCheck2)
-    {
-        return p;
-    }
-    else if(rootNodeCheck2 and !rootNodeCheck1)
-    {
-        return q;
-    }
-    else if(!rootNodeCheck1 and !rootNodeCheck2)
-    {
-        return nullptr;
-    }
-    else
-    {
-        return val;
-    }  
-        return nullptr;
+        return DFS(root,p,q);
     }
 };
