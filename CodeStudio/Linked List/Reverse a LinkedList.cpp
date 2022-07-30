fccce7fbcf96a701664d683c0ba445838009b51d
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-07-30 16:43:29 +0530

    Added solution of one problem.

---
+++ CodeStudio/Linked List/Reverse a LinkedList.cpp
@@ -0,0 +1,34 @@
+// Problem Link - https://www.codingninjas.com/codestudio/problems/799897?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int> * newHead = nullptr;
+    while(head!=nullptr)
+    {
+        LinkedListNode<int> * next = head->next;
+        head->next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
