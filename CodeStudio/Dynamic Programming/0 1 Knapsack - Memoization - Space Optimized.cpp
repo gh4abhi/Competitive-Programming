fca410835759f0d77381e2750867877c08cca3b3
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-11 21:34:54 +0530

    Added solution of one problem.

---
+++ CodeStudio/Dynamic Programming/0 1 Knapsack - Memoization - Space Optimized.cpp
@@ -0,0 +1,24 @@
+// Problem Link - https://www.codingninjas.com/codestudio/problems/0-1-knapsack_920542?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+#define ll long long
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight) 
+{
+    vector<ll> dp(maxWeight+1,0);
+    if(weight[0]<=maxWeight)
+        for(ll i=weight[0];i<=maxWeight;i++)
+            dp[i] = value[0];
+    for(ll i=1;i<n;i++)
+    {
+        vector<ll> temp = dp;
+        for(ll j=0;j<maxWeight+1;j++)
+        {
+            ll notTake = temp[j];
+            ll take = INT_MIN;
+            if(j>=weight[i])
+                take = max(take,value[i] + temp[j-weight[i]]);
+            dp[j] = max(take,notTake);
+        }
+    }
+    return dp[maxWeight];
+}
