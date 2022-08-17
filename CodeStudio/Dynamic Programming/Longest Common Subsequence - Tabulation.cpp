f69896cae65239d95bde04e27520bf582d7b4954
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-18 04:59:23 +0530

    Added solution of one problem.

---
+++ CodeStudio/Dynamic Programming/Longest Common Subsequence - Tabulation.cpp
@@ -0,0 +1,22 @@
+// Problem Link - https://www.codingninjas.com/codestudio/problems/longest-common-subsequence_624879?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+#include<bits/stdc++.h>
+#define ll int
+
+int lcs(string s, string t)
+{
+    ll m = s.length();
+    ll n = t.length();
+    vector<vector<ll>> dp(m+1,vector<ll>(n+1,0));
+    for(ll i=1;i<m+1;i++)
+    {
+        for(ll j=1;j<n+1;j++)
+        {
+            if(s[i-1]==t[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
