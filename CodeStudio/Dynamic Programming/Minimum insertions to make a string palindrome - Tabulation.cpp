d11cf63ad30dabba63230d66f6c7386a43db4406
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-18 06:48:06 +0530

    Added solution of one problem.

---
+++ CodeStudio/Dynamic Programming/Minimum insertions to make a string palindrome - Tabulation.cpp
@@ -0,0 +1,29 @@
+// Problem Link - https://www.codingninjas.com/codestudio/problems/minimum-insertions-to-make-palindrome_985293?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=0
+
+#include<bits/stdc++.h>
+#define ll int
+
+ll lcs(string s, string t)
+{
+    ll m = s.length(), n = t.length();
+    vector<ll> dp(m+1,0), temp(m+1);
+    for(ll i=1;i<m+1;i++)
+    {
+        temp = dp;
+        for(ll j=1;j<n+1;j++)
+        {
+            if(s[i-1]==t[j-1])
+                dp[j] = 1 + temp[j-1];
+            else
+                dp[j] = max(temp[j],dp[j-1]);
+        }
+    }
+    return dp[n];
+}
+
+int minInsertion(string &s)
+{
+    string t = s;
+    reverse(t.begin(),t.end());
+    return s.length() - lcs(s,t);
+}
