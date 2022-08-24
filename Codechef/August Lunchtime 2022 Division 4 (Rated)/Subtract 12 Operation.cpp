8b9f92bbc272d51bc93b2ad93fed18276a7c2c72
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-24 18:22:36 +0530

    Added solution of one problem.

---
+++ Codechef/August Lunchtime 2022 Division 4 (Rated)/Subtract 12 Operation.cpp
@@ -0,0 +1,26 @@
+// Problem Link - https://www.codechef.com/submit/SUB12OP?tab=statement
+
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+int n,a[200001],s;
+void solve() {
+  cin >> n;s=0;
+  for(int i=1;i<=n;++i) cin >> a[i];
+  for(int i=n;i>1;--i)
+    if(a[i]>0) {
+      a[i-1]-=a[i]/2;
+      a[i]=a[i]%2;
+      if(a[i]==1&&(a[i-1]%2==1||((i==2)&&a[1]>0))) {
+        a[i]=-1;
+        a[i-1]--;
+      }
+    }
+  for(int i=1;i<=n;++i) s+=abs(a[i]);
+  cout << s << endl;
+}
+signed main() {
+  int T;
+  cin >> T;
+  while(T--) solve();
+}
