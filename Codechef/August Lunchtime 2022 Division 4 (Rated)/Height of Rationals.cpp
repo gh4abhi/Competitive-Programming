86d66096c240aa6ca30065beab3f3f5a2d445e04
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-08-24 18:15:56 +0530

    Added solution of one problem.

---
+++ Codechef/August Lunchtime 2022 Division 4 (Rated)/Height of Rationals.cpp
@@ -0,0 +1,12 @@
+// Problem Link - https://www.codechef.com/submit/HEIGHTRATION
+
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<max(a,b);
+    return 0;
+}
