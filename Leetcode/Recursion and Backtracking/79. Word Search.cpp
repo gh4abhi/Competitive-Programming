08d09e6350e5bc61fe71a8af629708afed1a1252
Abhishek Swaroop Shrivastava <gh4abhi@gmail.com>
2022-09-12 23:54:12 +0530

    Added solution of one problem.

---
+++ Leetcode/Recursion and Backtracking/79. Word Search.cpp
@@ -0,0 +1,59 @@
+// Problem Link - https://leetcode.com/problems/word-search/
+
+#define ll int
+
+class Solution {
+public:
+    
+    bool DFS(vector<vector<char>>& board, string word, string op, vector<vector<ll>> &vis, ll i, ll j)
+    {
+        ll m = board.size();
+        ll n = board[0].size();
+        vis[i][j] = 1;
+        op += board[i][j];
+        if(op.length()>word.length())
+        {
+            vis[i][j] = 0;
+            return false;
+        }
+        if(op[op.length()-1]!=word[op.length()-1])
+        {
+            vis[i][j] = 0;
+            return false;
+        }
+        if(op==word)
+            return true;
+        if(i-1>=0 and vis[i-1][j]==0)
+            if(DFS(board,word,op,vis,i-1,j))
+                return true;
+        if(j-1>=0 and vis[i][j-1]==0)
+            if(DFS(board,word,op,vis,i,j-1))
+                return true;
+        if(i+1<m and vis[i+1][j]==0)
+            if(DFS(board,word,op,vis,i+1,j))
+                return true;
+        if(j+1<n and vis[i][j+1]==0)
+            if(DFS(board,word,op,vis,i,j+1))
+                return true;
+        vis[i][j] = 0;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        ll m = board.size();
+        ll n = board[0].size();
+        for(ll i=0;i<m;i++)
+        {
+            for(ll j=0;j<n;j++)
+            {
+                if(board[i][j]==word[0])
+                {
+                    vector<vector<ll>> vis(m,vector<ll>(n,0));
+                    if(DFS(board,word,"",vis,i,j))
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+};
