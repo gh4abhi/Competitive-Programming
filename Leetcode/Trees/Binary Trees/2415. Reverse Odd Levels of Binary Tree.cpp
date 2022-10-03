6d3e7ea876f27519511b6ccfd123712fba13bad9
gh4ahbi <gh4abhi@gmail.com>
2022-10-03 18:57:55 +0530

    Added solution of one problem.

--- Leetcode/Trees/Binary Trees/2415. Reverse Odd Levels of Binary Tree.cpp
+++ Leetcode/Trees/Binary Trees/2415. Reverse Odd Levels of Binary Tree.cpp
@@ -19,34 +19,36 @@ public:
         if(root==nullptr)
             return root;
         queue<TreeNode*> q;
+        stack<ll> st;
         q.push(root);
         q.push(nullptr);
         ll level = 0;
-        vector<TreeNode*> vect;
         while(q.size())
         {
             auto cur = q.front();
             q.pop();
             if(cur==nullptr)
             {
-                if(level&1)
-                {
-                    ll start = 0, end = vect.size()-1;
-                    while(start<end)
-                        swap(vect[start]->val,vect[end]->val), start+=1, end-=1;
-                }
                 if(q.size()==0)
                     break;
                 level+=1;
                 q.push(nullptr);
-                vect.clear();
                 continue;
             }
-            vect.push_back(cur);
+            if(level&1)
+                cur->val = st.top(), st.pop();
             if(cur->left)
+            {
+                if(!(level&1))
+                    st.push(cur->left->val);
                 q.push(cur->left);
+            }
             if(cur->right)
+            {
+                if(!(level&1))
+                    st.push(cur->right->val);
                 q.push(cur->right);
+            }
         }
         return root;
     }
