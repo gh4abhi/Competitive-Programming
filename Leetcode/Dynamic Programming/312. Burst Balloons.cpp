21f1abc11eb3f34a2b0aac25b75b1a8fe5952e2c
gh4ahbi <gh4abhi@gmail.com>
2022-10-10 03:34:18 +0530

    Added solution of one problem.

---
+++ Leetcode/Dynamic Programming/312. Burst Balloons.cpp
@@ -0,0 +1,24 @@
+// Problem Link - https://leetcode.com/problems/burst-balloons/
+
+#define ll int
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        ll n = nums.size();
+        vector<vector<ll>> dp(n+2,vector<ll>(n+2,0));
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        for(ll i=n;i>=1;i--)
+        {
+            for(ll j=1;j<=n;j++)
+            {
+                for(ll ind = i;ind<=j;ind++)
+                {
+                    dp[i][j] = max(dp[i][j],nums[i-1]*nums[j+1]*nums[ind] + dp[ind+1][j] + dp[i][ind-1]);
+                }
+            }
+        }
+        return dp[1][n];
+    }
+};
