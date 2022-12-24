3f246b7e9ec5c5f9cccce0038e210e7509776fdf
gh4abhi <gh4abhi@gmail.com>
2022-12-24 17:54:37 +0530

    Added solution of one prblem.

--- GeeksforGeeks/Graphs/Number of Distinct Islands.cpp
+++ GeeksforGeeks/Graphs/Number of Distinct Islands.cpp
@@ -4,32 +4,35 @@
 
 class Solution {
   public:
-    void DFS(vector<vector<ll>> &vect, ll m, ll n, ll i, ll j, vector<vector<ll>> &vis, vector<pair<ll,ll>> &ans, ll x, ll y)
+    
+    void DFS(ll i, ll j, vector<vector<ll>> &grid, vector<pair<ll,ll>> &vect, vector<vector<ll>> &vis, ll a, ll b)
     {
-        vis[i][j] = 1;
-        ans.push_back({i-x,j-y});
-        for(auto k:{-1,1})
-        {
-            if(i+k>=0 and i+k<m and vect[i+k][j]==1 and vis[i+k][j]==0)
-                DFS(vect,m,n,i+k,j,vis,ans,x,y);
-            if(j+k>=0 and j+k<n and vect[i][j+k]==1 and vis[i][j+k]==0)
-                DFS(vect,m,n,i,j+k,vis,ans,x,y);
-        }
-    }
-  
-    int countDistinctIslands(vector<vector<int>>& grid) {
-        set<vector<pair<ll,ll>>> s;
         ll m = grid.size();
         ll n = grid[0].size();
-        vector<vector<ll>> vis(m,vector<ll>(m,0));
+        vis[i][j] = 1;
+        vect.push_back({i-a,j-b});
+        for(auto x:{-1,1})
+        {
+            if(i+x>=0 and i+x<m and grid[i+x][j]==1 and vis[i+x][j]==0)
+                DFS(i+x,j,grid,vect,vis,a,b);
+            if(j+x>=0 and j+x<n and grid[i][j+x]==1 and vis[i][j+x]==0)
+                DFS(i,j+x,grid,vect,vis,a,b);
+        }
+    }
+    
+    int countDistinctIslands(vector<vector<int>>& grid) {
+        ll m = grid.size();
+        ll n = grid[0].size();
+        set<vector<pair<ll,ll>>> s;
+        vector<vector<ll>> vis(m,vector<ll>(n,0));
         for(ll i=0;i<m;i++)
         {
             for(ll j=0;j<n;j++)
             {
-                if(grid[i][j]==1 and vis[i][j]==0)
+                if(vis[i][j]==0 and grid[i][j]==1)
                 {
                     vector<pair<ll,ll>> vect;
-                    DFS(grid,m,n,i,j,vis,vect,i,j);
+                    DFS(i,j,grid,vect,vis,i,j);
                     s.insert(vect);
                 }
             }
